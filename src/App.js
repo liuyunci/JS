3422d082307c9eb75d0df20fdef6cb3b0fb16aa0
caichen <865216496@qq.com>
2020-05-14 20:04:27 +0800

    first modify

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React oh ye
+          Learn React oh yes
         </a>
       </header>
     </div>
